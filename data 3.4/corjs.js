6dd1f537148925659abb34729290d6afafcf5b82
h <jj@hh.com>
2023-04-03 19:20:44 +0200

    03042023

---
+++ data 3.4/corjs.js
@@ -0,0 +1,97 @@
+// // // // // // let x = "italia                  c  ";
+// // // // // // let y = "g  ";
+
+// // // // // // if (x.trim().length > y.length) {
+// // // // // //     console.log(x)
+// // // // // // } else if (x.trim().length < y.length) {
+// // // // // //     console.log(y)
+// // // // // // }
+
+
+
+// // // // // var x = 10;
+// // // // // var y = 9;
+// // // // // var z = 8;
+// // // // //  const somma = x + y + z;
+
+
+// // // // // console.log(somma)
+// // // // // console.log(x - y - z)
+
+
+
+// // // // // operatore MOD 
+
+// // // // var x = 11;
+// // // // var y = 2;
+// // // // var z = null
+
+// // // // z = x % y;
+
+// // // // if (z === 0) {
+// // // //     console.log("si!")
+// // // // } else {
+// // // //     console.log("no!")
+// // // // }
+
+
+// // // // string 
+
+// // // var x = "ciao"
+// // // var y = "mamma"
+
+// // // z = x + " " + y;
+// // // console.log(z)
+
+// // // 
+// // var x = 10;
+// // var y = "ciao"
+
+// // console.log(y - x)
+// // debug (non) perche non e un numero
+
+
+
+// var x = 10;
+// var y = 20;
+// var z = 30;
+
+// var w = x;
+
+
+// x = z;
+// z = y;
+// y = w;
+
+// console.log(x)
+// console.log(y)
+// console.log(z)
+// // 
+
+
+var x = 2;
+var y = 7;
+var z = null
+var q = null
+
+
+z = y + x;
+
+if (z >= 100) {
+    console.log("evai")
+
+}
+
+q = y % x;
+
+if (q === 0) {
+    console.log("yeee")
+
+} else if (q === 1) {
+    console.log("ciao")
+
+
+} else {
+    console.log("no")
+
+}
